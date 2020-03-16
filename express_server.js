017d60784ba9af41ab10c8da1ff2ced914de6451
Brent <brentcarey1@outlook.com>
2020-03-16 22:58:20 +0000

    Added another page to show shortURL

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls.json", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
+
+  app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+    res.render("urls_show", templateVars);
+  });
   
 
 app.listen(PORT, () => {
