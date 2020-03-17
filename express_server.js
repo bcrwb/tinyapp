f701acdf98e5c72e31f051a67732201bfa08b645
Brent <brentcarey1@outlook.com>
2020-03-17 20:01:41 +0000

    Added delete buttons and route

--- express_server.js
+++ express_server.js
@@ -60,6 +60,10 @@ app.get("/urls.json", (req, res) => {
     res.redirect(longURL);
   });
   
+  app.post("/urls/:shortURL/delete", (req, res) => {
+     delete urlDatabase[req.params.shortURL]
+    res.redirect(`/urls`);         
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
